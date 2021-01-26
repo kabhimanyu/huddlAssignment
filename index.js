1eb8f8523579a4c06672a1ab41c3732ee2cc9375
Abhimanuyu khosla <khoslaabhimanyu01@gmail.com>
2021-01-26 23:06:58 +0530

    Adding Error handling middleware for express and custom Error classes

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ require('module-alias/register')
 const { port, env } = require('@config/vars');
 const logger = require('@config/logger');
 const mongoose = require('@config/mongoose');
-const app = require('@config/express');
-
+const app = require('@config/express')
 mongoose.connect();
 
 app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
--- src/config/express.js
+++ src/config/express.js
@@ -4,6 +4,7 @@ const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
 const methodOverride = require('method-override');
+const error = require('@middlewares/error');
 
 const app = express();
 
@@ -17,5 +18,14 @@ app.use(methodOverride());
 app.use(helmet());
 
 app.use(cors());
+
+// if error is not an instanceOf APIError, convert it.
+app.use(error.converter);
+
+// catch 404 and forward to error handler
+app.use(error.notFound);
+
+// error handler, send stacktrace only during development
+app.use(error.handler);
 
 module.exports = app;
